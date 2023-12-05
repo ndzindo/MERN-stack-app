a758ee5ce371ac07b1b701dd780063ac596727b4
Nedim Džindo <dzindonedim@gmail.com>
2023-12-05 11:46:43 +0100

    Routes and DB set

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose'); 
+const workoutRoutes = require('./routes/workouts'); 
+
+//express app
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use((req,res,next) => {
+   console.log(req.path, req.method);
+   next();
+})
+
+app.use('/api/workouts',workoutRoutes);
+
+//db connection
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        //listen to requests
+        app.listen(process.env.PORT, () => {
+            console.log('listening on port',process.env.PORT);
+        })
+        
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+
